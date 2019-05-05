826925c11b7a70150044112825d161b06a346da0
guyibin <guyibin@withub.net.cn>
2019-05-05 10:46:59 +0800

    登录页样式调整

--- par-web/src/main/web/admin/src/routes/login/index.js
+++ par-web/src/main/web/admin/src/routes/login/index.js
@@ -38,45 +38,46 @@ const Login = ({
 
   return (
     <div className={styles.login}>
-      <img src={require('../../assets/login_bg.jpg')}/>
-      <h1/>
-      <h3>{title}</h3>
-      <div className={styles.form}>
-        <div className={styles.logo}>
-          <span>用户登录</span>
+      <div className={styles.main}>
+        <h3>{title}</h3>
+        <div className={styles.form}>
+          <div className={styles.logo}>
+            <span>用户登录</span>
+          </div>
+          <form>
+            {login.errorMessage && !loading.effects.login && renderMessage(login.errorMessage)}
+            <FormItem hasFeedback>
+              {getFieldDecorator('username', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入帐号',
+                  },
+                ],
+              })(<Input onPressEnter={handleOk} autoFocus placeholder="用户名"/>)}
+            </FormItem>
+            <FormItem hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '请输入密码',
+                  },
+                ],
+              })(<Input type="password" onPressEnter={handleOk} placeholder="密码"/>)}
+            </FormItem>
+            <div>
+              <Button type="primary" onClick={handleOk} loading={loading.effects.login}>
+                登录
+              </Button>
+            </div>
+            <div style={{textAlign: 'right', marginTop: '15px'}}>
+              {/*<a>忘记密码</a>*/}
+            </div>
+          </form>
         </div>
-        <form>
-          {login.errorMessage && !loading.effects.login && renderMessage(login.errorMessage)}
-          <FormItem hasFeedback>
-            {getFieldDecorator('username', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入帐号',
-                },
-              ],
-            })(<Input onPressEnter={handleOk} placeholder="用户名"/>)}
-          </FormItem>
-          <FormItem hasFeedback>
-            {getFieldDecorator('password', {
-              rules: [
-                {
-                  required: true,
-                  message: '请输入密码',
-                },
-              ],
-            })(<Input type="password" onPressEnter={handleOk} placeholder="密码"/>)}
-          </FormItem>
-          <div>
-            <Button type="primary" onClick={handleOk} loading={loading.effects.login}>
-              登录
-            </Button>
-          </div>
-          <div style={{textAlign: 'right', marginTop: '15px'}}>
-            {/*<a>忘记密码</a>*/}
-          </div>
-        </form>
       </div>
+
       <footer>{footerText}</footer>
     </div>
   )
